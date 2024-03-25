ff5de367437332ac4f320f93481b2128cbb51e7d
kori6811 <mr.sirojiddin4016810@gmail.com>
2024-03-25 16:02:44 +0900

    Google analytics is added

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { AnimatePresence } from 'framer-motion'
 import Chakra from '../components/chakra'
 import Payhip from '../components/payhip'
 import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
 
 if (typeof window !== 'undefined') {
   window.history.scrollRestoration = 'manual'
@@ -11,24 +12,39 @@ if (typeof window !== 'undefined') {
 
 function Website({ Component, pageProps, router }) {
   return (
-    <Chakra cookies={pageProps.cookies}>
-      <Fonts />
-      <Payhip />
-      <Layout router={router}>
-        <AnimatePresence
-          mode="wait"
-          initial={true}
-          onExitComplete={() => {
-            if (typeof window !== 'undefined') {
-              window.scrollTo({ top: 0 })
-            }
-          }}
-        >
-          <Component {...pageProps} key={router.route} />
-        </AnimatePresence>
-        <Analytics />
-      </Layout>
-    </Chakra>
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-JXFN3HZTBG"
+      />
+      <Script id="google-analytics">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-JXFN3HZTBG');
+          `}
+      </Script>
+      <Chakra cookies={pageProps.cookies}>
+        <Fonts />
+        <Payhip />
+        <Layout router={router}>
+          <AnimatePresence
+            mode="wait"
+            initial={true}
+            onExitComplete={() => {
+              if (typeof window !== 'undefined') {
+                window.scrollTo({ top: 0 })
+              }
+            }}
+          >
+            <Component {...pageProps} key={router.route} />
+          </AnimatePresence>
+          <Analytics />
+        </Layout>
+      </Chakra>
+    </>
   )
 }
 
